5033bb93543d5c4dc9f07869d7bdb41740b3e300
luchovs <lucianosavia15@gmail.com>
2025-03-31 12:03:50 -0300

    Agregué una nueva ruta

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("hello world")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("hola mundo")
+def hola_mundo():
+    return "<p>hola mundo</p>"
