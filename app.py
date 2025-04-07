239156fb61933799f547291bfd325d58d261ee1a
luchovs <lucianosavia15@gmail.com>
2025-04-07 09:50:26 -0300

    Agregamos una ruta usando argumentos

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("hello world")
+@app.route("/hello world")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("hola mundo")
+@app.route("/hola mundo")
 def hola_mundo():
     return "<p>hola mundo</p>"
+
+@app.rout("/tirar-dado/<int:caras>")
+def caras():
+    from random import randint
+    n = randint(1,caras)
+    return f"<p>Tire un dado de {caras} caras, salio {n}<p>"
