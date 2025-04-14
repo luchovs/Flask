6498c83a7cba080162e1ac2e5c1cb819e953192b
luchovs <lucianosavia15@gmail.com>
2025-04-14 12:00:21 -0300

    Hicimos una pagina usando Python y SQL en conjunto para agregar y ver a los demas usuarios, con y sin argumentos

--- app.py
+++ app.py
@@ -1,6 +1,44 @@
 from flask import Flask, url_for
+import sqlite3
 
 app = Flask(__name__)
+
+db = None
+def abrirConexion():
+    global db
+    db = sqlite3.connect("instance/datos.sqlite")
+
+def dict_factory(cursor, row):
+    """Arma un diccionario con los valores de la fila."""
+    fields = [column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields, row)}
+
+def cerrarConexion():
+    global db
+    db.close()
+    db.row_factory = dict_factory
+
+@app.route("/test-db")
+def testDB():
+    abrirConexion()
+    cursor = db.cursor()
+    cursor.execute("SELECT COUNT(*) AS cant FROM usuarios;")
+    res = cursor.fetchone
+    registros = res["cant"]
+    cerrarConexion()
+    return f"Hay {registros} registros en la tabla usuarios"
+
+@app.route("/crear-usuario")
+def testCrear():
+    nombre = "leandro"
+    email = "leandro@etec.uba.ar"
+    abrirConexion()
+    cursor = db.cursor()
+    consulta = "INSERT INTO usuarios (usuario, email) VALUES (?, ?);"
+    cursor.execute(consulta, (nombre, email))
+    db.commit()
+    cerrarConexion()
+    return f"Registro agregado ({nombre})"
 
 @app.route("/")
 def main():
